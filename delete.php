8e9ff71b9f8c45c787dddaac593a4e3d830719f0
BrunoCavalcantNunes <contatwo234@gmail.com>
2021-11-06 21:54:45 +0000

    header footer

---
+++ delete.php
@@ -0,0 +1,40 @@
+<?php
+require_once './vendor/autoload.php';
+
+use TrabalhoPDOMySQL\MySQLConnection;
+
+$bd = new MySQLConnection();
+
+$genero = null;
+
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $comando = $bd->prepare('SELECT * FROM genero WHERE id = :id');
+    $comando->execute([':id' => $_GET['id']]);
+
+    $genero = $comando->fetch(PDO::FETCH_ASSOC);
+
+} else {
+    $comando = $bd->prepare('DELETE FROM generos WHERE id = :id WHERE id = :id');
+    $comando->execute([':id'=> $_POST['id']]);
+
+    header('Location:/index.php');
+}
+
+
+
+$_title = 'Excluir Gênero';
+
+?>
+
+<?php include('./includes/header.php'); ?>
+
+    <h1>Remover Gênero</h1>
+    <p>Tem certeza que deseja remover este gênero <?= $genero['nome'] ?> ?</p>
+
+    <form action="delete.php" method="post">
+        <input type="hidden" name="id" value="<?= $genero['id'] ?>" />
+        <a class="btn btn-secondary" href="index.php">Voltar</a>
+        <button class="btn btn-danger" type="submit">Excluir</button>
+        </form>
+
+<?php include('./includes/footer.php'); ?>
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+require_once './vendor/autoload.php';
+//use PDO;
+
+use TrabalhoPDOMySQL\MySQLConnection; //PDO
+$bd = new MYSQLConnection(); //PDO ('mysql:host=localhost;dbname=biblioteca', 'root', '');
+
+$comando = $bd->prepare('SELECT * FROM generos');
+$comando->execute();
+$generos = $comando->fetchAll(PDO::FETCH_ASSOC);
+
+$_title = 'Gêneros';
+
+?>
+
+<?php include('./includes/header.php'); ?>
+
+        <a class="btn btn-primary" href="insert.php">Novo Gênero</a>
+        <table class="table">
+             <tr>
+                <th>Id</th>
+                 <th>Nome</th>
+                <th>&nbsp;</th>
+             </tr>
+             <?php foreach($generos as $g): ?>
+                <tr>
+                    <td><?= $g['id']?></td>
+                     <td><?= $g['nome']?></td>
+                     <td>
+                         <a class="btn btn-secondary" href="update.php?id=<?= $g['id'] ?>">Editar</a>
+                         <a class="btn btn-danger" href="delete.php?id=<?= $g['id'] ?>">Excluir</a>
+                    </tr>
+                <?php endforeach ?>
+            </table>
+<?php include('./includes/footer.php'); ?>
+
+
